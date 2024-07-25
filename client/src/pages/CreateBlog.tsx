a09f4ebd7714ff27bde06b2a07c90e2da16ba147
victory benson <victorybenson98@gmail.com>
2024-07-25 02:46:39 +0100

    update createblog page

--- client/src/pages/CreateBlog.tsx
+++ client/src/pages/CreateBlog.tsx
@@ -1,23 +1,24 @@
 import Wrapper from "../shared/Wrapper"
+import { AiOutlineCloudUpload } from "react-icons/ai";
 
 const CreateBlog = () => {
   return (
     <Wrapper>
-        <div className="max-h-screen flex justify-center">
-            <form className="rounded-lg h-inherit md:w-[80%] shadow-lg flex flex-col items-center my-2">
-                <div className="h-40 w-full md:w-[80%] my-2 rounded border overflow-hidden">
-                    <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" className="object-contain w-full h-full" alt="image file" />
+        <div className="max-h-screen flex justify-center md:m-10 p-2 rounded-lg ">
+            <form className=" h-inherit md:w-[80%] flex flex-col items-center ">
+                <div className="h-40 w-full md:w-[80%] cursor-pointer my-2 rounded border-dashed border-2 border-darkGray overflow-hidden flex justify-center items-center">
+                    <div className="flex items-center text-xl text-darkGray h-inherit"><AiOutlineCloudUpload />click to upload image</div>
                 </div>
                 <div className="flex flex-col gap-2 items-center justify-center my-2">
-                    <div className="w-full md:w-auto">
-                        <input type="text" placeholder="blog title" className="border p-2 rounded w-full"/>
+                    <div className="w-full md:w-2/3">
+                        <input type="text" placeholder="blog title" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                     </div>
                     <div className="gap-4 flex flex-wrap items-center justify-center">
                         <div className="w-full md:w-auto">
-                            <input type="text"  placeholder="author" className="border p-2 rounded w-full flex"/>
+                            <input type="text"  placeholder="author" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                         </div>
                         <div className="w-full md:w-auto">
-                            <input list="labels"  placeholder="select label" className="border p-2 rounded w-full"/>
+                            <input list="labels"  placeholder="select label" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                             <datalist id="labels">
                                 <option value="technology"></option>
                                 <option value="gadget"></option>
@@ -26,7 +27,7 @@ const CreateBlog = () => {
                             </datalist>
                         </div>
                         <div className="w-full md:w-auto">
-                            <input list="readTime"  placeholder="select read time" className="border p-2 rounded w-full"/>
+                            <input list="readTime"  placeholder="select read time" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                             <datalist id="readTime">
                                 <option value="1"></option>
                                 <option value="2"></option>
@@ -36,10 +37,14 @@ const CreateBlog = () => {
                             </datalist>
                         </div>
                     </div>
-                    <textarea name="content" id="" rows={10} className="border w-full p-2" placeholder="tell your story"></textarea>
+                    <div className="w-full">
+                        <textarea name="content" id="" rows={10} className=" w-full p-2 ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40" placeholder="Tell your story"></textarea>
+                    </div>
                 </div>
-                <div className="w-full md:w-[80%] my-5 rounded">
-                    <button className="w-full p-3 bg-blue1 text-white rounded">Publish</button>
+                <div className=" md:w-[80%] w-full my-5 rounded">
+                    <button className="w-full p-3 bg-blue1 text-white rounded-lg hover:bg-opacity-80 transition-all duration-300 flex items-center justify-center">
+                        <AiOutlineCloudUpload />Publish
+                    </button>
                 </div>
             </form>
         </div>
