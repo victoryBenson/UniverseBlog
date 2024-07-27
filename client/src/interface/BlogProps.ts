5fb80bc695ac4699484da017efedcd2d49baa2a3
victory benson <victorybenson98@gmail.com>
2024-07-27 21:55:51 +0100

    minor-update

--- client/src/interface/BlogProps.ts
+++ client/src/interface/BlogProps.ts
@@ -7,18 +7,11 @@ export interface BlogProps {
     author: string;
     label: string;
     readTime: string;
-    image: string;
+    image?: string;
     createdAt: string;
     updatedAt: string;
   }
 
 
-  export interface FormInitialState {
-    author:string,
-    tittle:string,
-    content:string,
-    readTime:string,
-    label:string,
-    image: null | File
-}
-
+//new blog prop
+export type NewBlog = Omit<BlogProps, "_id" | "createdAt" | "updatedAt">;
--- client/src/pages/CreateBlog.tsx
+++ client/src/pages/CreateBlog.tsx
@@ -1,27 +1,29 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import Wrapper from "../shared/Wrapper"
 import { AiOutlineCloudUpload } from "react-icons/ai";
-import { BlogProps, FormInitialState } from "../interface/BlogProps";
-import axiosInstance from "../utils/AxiosConfig";
+import { NewBlog } from '../interface/BlogProps';
+import { UseData } from "../context/Blog";
 import toast from "react-hot-toast";
-import { title } from "process";
+import { useNavigate } from "react-router-dom";
 
 
-
-const initialState: FormInitialState = {
+const initialState: NewBlog = {
     author: "",
-    tittle: "",
+    title: "",
     content: "",
     readTime: "",
     label: "",
-    image: null
+    image: "",
 }
 
 const CreateBlog = () => {
-    const [formState, setFormState] = useState<FormInitialState>(initialState);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [isError, setIsError] = useState<undefined | string>(undefined);
-    const {author, tittle, content, readTime, label, image} = formState;
+    const [formState, setFormState] = useState<NewBlog>(initialState);
+    const {author, title, content, readTime, label, image} = formState;
+    const {createBlog} = UseData();
+    const [loading, setLoading] = useState<boolean>(false)
+    const [isError, setIsError] = useState<null | string>()
+    const formRef = useRef<HTMLFormElement>(null);
+    const navigate = useNavigate()
   
 
    
@@ -33,18 +35,18 @@ const CreateBlog = () => {
     const handleSubmit = async(event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         setLoading(true);
-
         try {
-            
-            
-            return await response.data
+            const BlogData = {author, title, content, readTime, label, image}
+            await createBlog(BlogData)
+            toast.success('Blog created Successfully!')
+            navigate("/")
         } catch (error) {
             if(error instanceof Error){
                 setIsError(error.message)
             }else{
                 setIsError("An unknown error occurred")
             }
-        } finally {
+        }finally{
             setLoading(false)
         }
     };
@@ -53,27 +55,18 @@ const CreateBlog = () => {
   return (
     <Wrapper>
         <div className="max-h-screen flex justify-center md:m-10 p-2 rounded-lg ">
-            <form onSubmit={handleSubmit} className=" h-inherit md:w-[80%] flex flex-col items-center ">
+            <form ref={formRef} onSubmit={handleSubmit} className=" h-inherit md:w-[80%] flex flex-col items-center ">
                 <div className="h-40 w-full md:w-[80%] cursor-pointer my-2 rounded border-dashed border-2 border-darkGray overflow-hidden flex justify-center items-center">
-                    {/* <label htmlFor="" >
-                        <input type="file" />
-                    </label>
-                    { image? (
-                        <div>
-                            <img src={image} alt="image" />
-                        </div>
-                    ) : (
-                        <div className="flex items-center text-xl text-darkGray h-inherit"><AiOutlineCloudUpload />click to upload image</div>
-                    )} */}
                      <input
                         type="file"
+                        accept='image/*'
                         name="image"
                         onChange={handleChange}
                     />
                 </div>
                 <div className="flex flex-col gap-2 items-center justify-center my-2">
                     <div className="w-full md:w-2/3">
-                        <input type="text" name="title" value={tittle} onChange={handleChange} placeholder="blog title" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
+                        <input type="text" name="title" value={title} onChange={handleChange} placeholder="blog title" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                     </div>
                     <div className="gap-4 flex flex-wrap items-center justify-center">
                         <div className="w-full md:w-auto">
@@ -91,11 +84,11 @@ const CreateBlog = () => {
                         <div className="w-full md:w-auto">
                             <input list="readTime" name="readTime" onChange={handleChange}  placeholder="select read time" className="p-2 w-full ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40"/>
                             <datalist id="readTime">
-                                <option value="1"></option>
-                                <option value="2"></option>
-                                <option value="3"></option>
-                                <option value="4"></option>
-                                <option value="5"></option>
+                                <option value="1min"></option>
+                                <option value="2min"></option>
+                                <option value="3min"></option>
+                                <option value="4min"></option>
+                                <option value="5min"></option>
                             </datalist>
                         </div>
                     </div>
@@ -103,8 +96,9 @@ const CreateBlog = () => {
                         <textarea name="content" value={content} onChange={handleChange} rows={10} className=" w-full p-2 ring-1 ring-blue2/20 rounded-xl focus:ring-2 focus:outline-blue1/60 bg-arch/40" placeholder="Tell your story"></textarea>
                     </div>
                 </div>
+                <div className="text-red">{isError && isError }</div>
                 <div className=" md:w-[80%] w-full my-5 rounded">
-                    <button className="w-full p-3 bg-blue1 text-white rounded-lg hover:bg-opacity-80 transition-all duration-300 flex items-center justify-center">
+                    <button disabled={loading} className="w-full p-3 bg-blue1 text-white rounded-lg hover:bg-opacity-80 transition-all duration-300 flex items-center justify-center">
                         <AiOutlineCloudUpload />Publish
                     </button>
                 </div>
