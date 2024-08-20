20aec5dba2f60b53a315d52bd4908c3e325500c5
victoryBenson <victoryBenson98@gmail.com>
2024-08-20 08:09:58 +0100

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,8 +26,10 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true
+    optionsSuccessStatus : 200,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    credentials: true,
+    preflightContinue: false
 }));
 
 
