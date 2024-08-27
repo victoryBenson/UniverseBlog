afcbbfd2b6f52762707eba4b89859b957a52b270
victoryBenson <victoryBenson98@gmail.com>
2024-08-27 10:26:11 +0100

    minor-fix

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -97,49 +97,49 @@ export const Register = () => {
                 <Link to={"/"} className="pl-4 my-3 flex items-center absolute bottom-4 gap-2 text-lightGray font-light cursor-pointer"><FaArrowRightLong size={30} className="animate-pulse "/>continue without login!</Link>
             </div>
             <div className='bg-white min-h-full flex justify-center flex-col items-center relative p-5'>
-                <form onSubmit={handleSubmit} className="w-full md:w-1/2 lg:w-2/3">
-                    <h1 className='font-bold text-3xl py-3 hidden lg:block text-center'>Create Account?</h1>
+                <form onSubmit={handleSubmit} className="px-5 md:px-0 w-full md:w-1/2 lg:w-2/3">
+                    <h1 className='font-bold text-xl hidden lg:block text-center'>Create Account?</h1>
                     <div className="text-center lg:hidden">
                         <div className="flex items-center justify-center ">
                             <GiMoebiusTriangle className=" rounded"/>
                         </div>
                         <h1 className='font-extrabold text-base md:text-2xl'>Create Account?</h1>
                         <h4 className='text-sm sm:text-base text-center '>Sign up to get access to members only content</h4>
-                        <button className="flex lg:hidden w-full my-3 items-center justify-center p-3 rounded-full border border-lightGray/40 text-lg gap-2"><FcGoogle size={30}/>Sign up with Google</button>
-                        <span className="text-base flex lg:hidden justify-center">or</span>
+                        <button className="flex lg:hidden w-full my-3 items-center justify-center p-3 rounded-full border border-lightGray/40 text-base lg:text-lg gap-2"><FcGoogle/>Sign up with Google</button>
+                        <span className="text-sm flex lg:hidden justify-center">or</span>
                     </div>
-                    <div className="flex flex-col items-start justify-start py-1 relative">
-                        <label htmlFor="username" className="text-base md:text-lg">Username</label>
+                    <div className="flex flex-col items-start justify-start relative">
+                        <label htmlFor="username" className="text-base">Username</label>
                         <input 
                             type="text" 
                             autoFocus 
-                            className="required border-b border-lightGray/80 outline-none text-xl w-full pl-7 p-2" 
-                            placeholder="Kennytech"
+                            className="required border-b border-lightGray/80 outline-none text-base w-full pl-7 p-2" 
+                            placeholder="John Doe"
                             name="username"
                             value={username}
                             onChange={handleChange} 
                             required
                         />
-                        <span className="absolute left-0 bottom-5 text-darkGray"><FaRegUser size={20}/></span>
+                        <span className="absolute left-0 bottom-4 text-darkGray"><FaRegUser/></span>
                     </div>
                     <div className="flex flex-col items-start justify-start py-1 relative">
-                        <label htmlFor="email" className="text-base md:text-lg">Email</label>
+                        <label htmlFor="email" className="text-base">Email</label>
                         <input 
                             type="email" 
-                            className="required  border-b border-lightGray/80 outline-none text-xl w-full pl-7 p-2" 
+                            className="required  border-b border-lightGray/80 outline-none text-base w-full pl-7 p-2" 
                             placeholder="example@gmail.com" 
                             name="email"
                             value={email}
                             onChange={handleChange}
                             required
                         />
-                        <span className="absolute left-0 bottom-5 text-darkGray"><MdOutlineMail size={20}/></span>
+                        <span className="absolute left-0 bottom-4 text-darkGray"><MdOutlineMail/></span>
                     </div>
-                    <div className="flex flex-col items-start justify-start py-1 relative">
-                        <label htmlFor="password" className="text-base md:text-lg">Password</label>
+                    <div className="flex flex-col items-start justify-start relative">
+                        <label htmlFor="password" className="text-base">Password</label>
                         <input 
                             type={viewPwd? "text" : "password"} 
-                            className=" border-b border-lightGray/80 outline-none text-xl w-full pl-7 p-2" 
+                            className=" border-b border-lightGray/80 outline-none text-base w-full pl-7 p-2" 
                             placeholder="**********"
                             name="password"
                             value={password}
@@ -147,13 +147,13 @@ export const Register = () => {
                             required
                         />
                         <span onClick={togglePwd} className='absolute top-[54%] right-10 text-brown transition-transform  duration-300 cursor-pointer text-darkGray'>{viewPwd? <LuEye /> : <LuEyeOff/>}</span>
-                        <span className="absolute left-0 top-1/2 text-darkGray"><LuLock size={20}/></span>
+                        <span className="absolute left-0 top-1/2 text-darkGray"><LuLock/></span>
                     </div>
-                    <div className="flex flex-col items-start justify-start py-1 relative">
-                        <label htmlFor="password" className="text-base md:text-lg">Confirm Password</label>
+                    <div className="flex flex-col items-start justify-start relative">
+                        <label htmlFor="password" className="text-base">Confirm Password</label>
                         <input 
                             type={viewConfirmPwd? "text" : "password"} 
-                            className=" border-b border-lightGray/80 outline-none text-xl w-full pl-7 p-2" 
+                            className=" border-b border-lightGray/80 outline-none text-base w-full pl-7 p-2" 
                             placeholder="**********"
                             name="confirmPassword"
                             value={confirmPassword}
@@ -161,9 +161,9 @@ export const Register = () => {
                             required
                         />
                         <span onClick={toggleConfirmPwd} className='absolute top-[54%] right-10 text-brown transition-transform  duration-300 cursor-pointer text-darkGray'>{viewConfirmPwd? <LuEye /> : <LuEyeOff/>}</span>
-                        <span className="absolute left-0 top-1/2 text-darkGray"><LuLock size={20}/></span>
+                        <span className="absolute left-0 top-1/2 text-darkGray"><LuLock/></span>
                     </div>
-                    <p className="gap-2 flex py-1 items-center"><input type="checkbox" required name="" id="" className="cursor-pointer" />I accept the Terms of Use.</p>
+                    <p className="gap-2 flex py-1 items-center text-sm"><input type="checkbox" required name="" id="" className="cursor-pointer" />I accept the terms of use.</p>
                     <span className="text-red text-sm flex">{isError && isError}</span>
                     <div>
                         <button type='submit' disabled={loading} className="flex bg-blue1 my-2 items-center justify-center p-3 rounded-full text-white w-full">
@@ -176,8 +176,8 @@ export const Register = () => {
                         </button>
                     </div>
                     <span className="text-sm hidden lg:flex justify-center">or</span>
-                    <button className="hidden lg:flex w-full my-3 items-center justify-center p-3 rounded-full border border-lightGray/40 text-lg gap-2"><FcGoogle size={30}/>Sign up with Google</button>
-                    <span className="pl-4 my-3 block py-3">Already have an account?<Link to={'/login'} className="text-blue1 px-2 hover:underline decoration-2 underline-offset-2">login</Link></span>
+                    <button className="hidden lg:flex w-full my-2 items-center justify-center p-3 rounded-full border border-lightGray/40 text-base gap-2 hover:shadow"><FcGoogle size={20}/>Sign up with Google</button>
+                    <span className=" lg:text-sm block lg:py-3">Already have an account?<Link to={'/login'} className="text-blue1 px-2 hover:underline decoration-2 underline-offset-2">login</Link></span>
                 </form>
             </div>
         </div>
